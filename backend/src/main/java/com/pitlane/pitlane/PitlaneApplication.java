c50d68b13baa4f58e684a0767bb85696115591b7
Miguel Maravilhas <miguel.maravilhas.silva@gmail.com>
2026-04-07 17:16:10 +0100

    feat: setup backend structure, swagger, flyway and react frontend

---
+++ backend/src/main/java/com/pitlane/pitlane/PitlaneApplication.java
@@ -0,0 +1,13 @@
+package com.pitlane.pitlane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PitlaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PitlaneApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/pitlane/pitlane/PitlaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pitlane.pitlane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PitlaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
